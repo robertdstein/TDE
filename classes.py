098d32a09419e66b4e9102fde42101d71d60db5f
Robert Stein <robert.stein@desy.de>
2017-07-21 10:46:28 +0200

    Backup 21/07/17

--- classes.py
+++ classes.py
@@ -1,14 +1,18 @@
 import zipfile
+import math
 import json
 import datetime
 import matplotlib
+matplotlib.use('Agg')
 import numpy as np
+from scipy import optimize
 from scipy.optimize import curve_fit
+from numpy import vectorize
 from astropy.time import Time
-from astropy import constants as const
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 minpoints = 3
 
@@ -18,6 +22,8 @@ def wrapAround180(ra_deg):
     return ra
 				
 def magtolum(absmag):
+	"""Convert an absolute magnitude to a distance.
+	"""
 	solarlum = 3.826 * 10**33
 	solarabsmag = 4.75
 	exponent = (solarabsmag - float(absmag))/2.5
@@ -38,6 +44,9 @@ class Full_set:
 		self.extract()
 		
 	def extract(self):
+		"""Extract all data from tde.zip.
+		The zipfile contains individual json files for each candidate.
+		"""
 		zf = zipfile.ZipFile(self.path, 'r')
 		self.creation_time=datetime.datetime(*zf.infolist()[0].date_time)
 		
@@ -70,6 +79,8 @@ class Full_set:
 		print "Catalogue version created:", self.creation_time, "\n"
 		
 	def info_candidate(self, name):
+		"""Plots all available light curves for a given candidate.
+		"""
 		if hasattr(self.TDEs, name):
 			tde = getattr(self.TDEs, name) 
 			print "TDE found, with photometry =", tde.has_photometry
@@ -78,15 +89,17 @@ class Full_set:
 			raise Exception("No TDE with this name was found!")
 				
 	def plot_all_candidates(self):
+		"""Plots all available light curves for each candidate.
+		"""
 		for name in vars(self.TDEs):
 			tde = getattr(self.TDEs, name)
 			tde.plot_candidate()
 			
 	def plot_all_bands(self):
+		"""Plots all available light curves for each band.
+		"""
 		for band in self.all_available_bands:
 			title = band
-			#~ print "Producing graph for band", band
-			
 			
 			folder = "bands/"
 			
@@ -118,21 +131,31 @@ class Full_set:
 			scatter_plot(folder, title, allt, ally, allupt, allupy, labels, variables, maxdate, sharex=True)
 		
 	def list_best(self):
+		"""Lists the TDE candidates which are most promising.
+		"""
 		best=[]
+		ratio=[]
 		print "\n The best candidates, in terms of available data, are: \n"
 		for name in vars(self.TDEs):
 			tde = getattr(self.TDEs, name)
 			if tde.best:
 				best.append(name)
 				print name
+			if tde.ratiotest:
+				ratio.append(name)
 		self.best_list = best
 		print "\n \n"
+		print "Those passing the ratio test are: \n"
+		print ratio
+		
 	
 	def plot_distibutions(self):
-		variables = ["redshift", "comovingdist", "lumdist", "hostoffsetdist", "mjdmax", "nbands","maxabsmag",  "nhhost", "ra_float", "dec_float"]
+		""" Plots binned distributions of candidates for each variable in 'variables'.
+		"""
+		variables = ["redshift", "comovingdist", "lumdist", "hostoffsetdist", "mjdmax", "nbands","maxabsmag",  "nhhost", "ra_float", "dec_float", "ebv"]
 		
-		titles = ["Redshift", "Comoving Distance", "Luminosity Distance", "Host Offset Distance", "Date of Max Luminosity", "Number of observational bands", "Max Absolute Magnitude", "Something cool, probably (nnhost)", "Right ascension", "Declination"]
-		xlabels = ["Redshift (z)", "Distance (MPc)", "Distance (MPc)", "Distance (?)", "Time(MJD)", "n", "", "???", "hours", "degrees"]
+		titles = ["Redshift", "Comoving Distance", "Luminosity Distance", "Host Offset Distance", "Date of Max Luminosity", "Number of observational bands", "Max Absolute Magnitude", "Something cool, probably (nnhost)", "Right ascension", "Declination", "EBV"]
+		xlabels = ["Redshift (z)", "Distance (MPc)", "Distance (MPc)", "Distance (?)", "Time(MJD)", "n", "", "???", "hours", "degrees","Magnitudes"]
 		
 		values = []
 		for i in range (0, len(variables)):
@@ -153,12 +176,17 @@ class Full_set:
 		histograms_plot(titles, xlabels, values)
 		
 	def plot_spectra(self):
+		"""Plots the spectrum for each candidate  which has one
+		"""
 		for name in vars(self.TDEs):
 			tde = getattr(self.TDEs, name)
 			if tde.has_spectra:
 				tde.plot_spectrum()
 				
 	def plot_skymap(self):
+		"""Plots a map of the distribution of TDE candidates on the sky.
+		Uses the redshift as a colour scale.
+		"""
 		ra=[]
 		dec = []
 		rs=[]
@@ -203,6 +231,7 @@ class TDE_Candidate:
 		self.name = jsonfile[0]["name"]
 		self.has_photometry = False
 		self.has_spectra = False
+		self.ratiotest = False
 		self.has_xray = False
 		self.best=False
 		for key, val in jsonfile[0].items():
@@ -222,6 +251,8 @@ class TDE_Candidate:
 		self.isbest()
 		
 	def coordinates(self):
+		"""Converts the coordinates given in the json files to floats.
+		"""
 		if hasattr(self, "ra") and hasattr(self, "dec"):			
 			self.ra_float = get_degrees(self.ra.value)
 			self.dec_float = get_degrees(self.dec.value)
@@ -231,6 +262,8 @@ class TDE_Candidate:
 			
 	
 	def isbest(self):
+		"""Assesses whether a given candiate is promising.
+		"""
 		self.check_xray()
 		if self.has_photometry:
 			if hasattr(self, "mjdmax") and hasattr(self, "mjdvismax"):
@@ -238,6 +271,9 @@ class TDE_Candidate:
 					self.best=True
 	
 	def mjd_time(self):
+		"""Converts the discovery date to MJD units.
+		Does the same for the maximum date, and maximum visual date.
+		"""
 		if hasattr(self, "discoverdate"):
 			val = self.discoverdate.value.split("/")
 			if len(val)==3:
@@ -260,6 +296,9 @@ class TDE_Candidate:
 			setattr(self, "mjdvismax", float(t.mjd))
 					
 	def group_photometry(self, val):
+		"""Extracts the photometry data from the json file, and groups it
+		into seperate claases for each available band.
+		"""
 		allbands = container()
 		bandlist = []
 		varlist=[]
@@ -306,69 +345,268 @@ class TDE_Candidate:
 				self.has_photometry = True
 				
 	def check_xray(self):
+		"""Checks to see if there is a light curve visible in X Ray measurements.
+		Fits this distribution to see if it is close to the charecteristic t^-5/3.
+		"""
 		xrband = "X-Ray (0.3-2.0) keV"
 		if hasattr(self, "bandlist"):
 			if xrband in self.bandlist:
-				time, y, upt, upy, md = self.return_photometry_dataset(xrband, "luminosity")
-				if (len(time) + len(upt) > 2) and (time != []):
-#					print self.name
+				data = getattr(self.photometry, xrband+"_"+"luminosity")					
+				
+				time=[]
+				y=[]
+				sigma=[]
+				upt = []
+				upy =[]
+				upsig=[]
+				lowersig = []
+				
+				for entry in data:
+					if "time" in entry.keys():
+						t = entry["time"]
+						if isinstance(t, list):
+							t = t[0]
+						if "upperlimit" in entry.keys():
+							upy.append(float(entry["luminosity"]))
+							upt.append(float(t))
+						else:
+							lum = float(entry["luminosity"])
+							y.append(lum)
+							time.append(float(t))
+							meansigma = 0.5*(float(entry["e_upper_luminosity"]) + float(entry["e_lower_luminosity"]))
+							if meansigma==0.0:
+								meansigma = 0.5*lum
+								upsig.append(0.5*lum)
+								lowersig.append(0.5*lum)
+								
+							sigma.append(meansigma)
+							upsig.append(float(entry["e_upper_luminosity"]))
+							lowersig.append(float(entry["e_lower_luminosity"]))
 					
-					if hasattr(self, "maxabsmag") & hasattr(self, "mjdmax"):
-						maxabs = self.maxabsmag.value
-						maxlum = magtolum(maxabs)
-#						print "Max Lum", maxlum, max(y), "Mag", maxabs
-						maxtime = self.mjdmax
+				if len(y) > 0:
+					maxlum = float(max(y))
+					maxtime = time[y.index(maxlum)]
 					
-					else:
-						maxlum = float(max(y))
-						maxtime = time[y.index(maxlum)]				
+					print self.name, maxlum, y, maxtime, time
 					
-					ratiotest=False					
+					allt = time+upt
 					
-					lum = maxlum
-#					tbest = 0.0
+					lastt=0.0			
 					
+					for j in range(len(allt)):
+						t = allt[j]
+						if lastt < t < maxtime:
+							lastt=t
+							
+					preceding = []
 					for i in range(len(upt)):
 						t = upt[i]
-						if t < (maxtime - 20):
-							lum = upy[i]
-#							print lum	, t
+						if float(t) < (maxtime-50):
+							preceding.append(float(upy[i]))
+							
+#					print preceding, "Max lum:", maxlum
 					
-#					print maxlum, maxtime, "\n \n", lum, "\n \n"
+					if len(preceding) > 0:
+						rmax = float(maxlum)/float(max(preceding))					
+						rmean = float(maxlum)/float(np.mean(preceding))
+						rmin= float(maxlum)/float(min(preceding))
+						
+						if  rmax> 1.5:
+							if rmean > 4:
+								if rmin > 20:
+									self.ratiotest=True
+								
+#						print self.name, rmax, rmean, rmin, self.ratiotest
+#					else:
+#						print self.name, "has insufficient data"
 					
-					if float(maxlum)/float(lum) > 100:
-						ratiotest = True
-					
-					if ratiotest:
-						print self.name, "has passed the ratiotest", float(maxlum)/float(lum)
-					
+#					if self.ratiotest:
+#						print self.name, "has passed the ratio test!"
+#					else:
+#						print self.name, "has FAILED the ratio test!"
+						
 					fitt=[]
 					fity=[]
+					fitsig=[]
+					fitup=[]
+					fitdown=[]
 					for i in range(len(time)):
 						t = time[i]
-						if float(t) > maxtime:
-							fitt.append(np.log(t - maxtime))
-							fity.append(np.log(float(y[i])))
-						
-								
-#					if ratiotest:
-#						print self.name, "Found measurements before maximum at least two orders of magnitude smaller"
-					
+						if float(t) >= maxtime:
+							fitt.append(float(t))
+							fity.append(float(y[i]))
+							fitsig.append(sigma[i])
+							fitup.append(upsig[i])
+							fitdown.append(lowersig[i])
+							
+							
 					if len(fitt) > 2:
-#						print fitt, fity
 						
-						def curve(x, a, b):
-							return x* a + b
+						fig = plt.figure()	
+						ax1 = plt.subplot(312)
 						
-						popt, pcov = curve_fit(curve, fitt, fity, method="trf", bounds=(0, np.inf))
-						print popt, pcov
+						if hasattr(self, "mjdmax"):
+							plt.axvline(self.mjdmax, color="k", label="max date")
+							
+						if hasattr(self, "mjdmax"):
+							plt.axvline(self.mjdvismax, color = "green", label="max vis date")
+							
+						plt.suptitle(self.name)
+						ax1.set_yscale('log')
+						
+						fitt = np.array(fitt)
+						fity = np.array(fity)
+						fitsig = np.array(zip(fitup, fitdown))
+						
+#						print fity, fitt, fitup, fitdown
+#						raw_input("prompt")
+						
+						ax1.errorbar(fitt, fity, yerr=[fitup, fitdown], label="measurements", color="b", ecolor="r", fmt='o')
+						plt.xlabel("t")
+						plt.ylabel("Log(Luminosity)")
+						plt.legend()
+						
+						params=[]
+						covars = []
+						vals = []
+						
+						scale = np.log10(maxtime  - lastt)			
+						
+						offset = np.logspace(0, scale, num=200)
+#						offset = np.array(range())
+#						print "Range", offset
+						
+						for i in offset:
+							starttime = maxtime - (i + 1e-5)
+#							if hasattr(self, "mjdmax"):
+#								starttime = self.mjdmax
+							
+							newt = fitt-starttime
+							logfitt = np.log10(newt)
+							
+							def indices(y,model):
+								if y > model:
+									return 0
+								else:
+									return 1
+									
+							vfunc = vectorize(indices)
+							
+							
+							def fitfunc(p, x):
+								return (p[0]*(x**p[1]))
+								
+							def err(indices, errs):
+								return errs[indices]
+								
+							verr = vectorize(err)
+								
+							def llh(p, x, y, err2):
+								model = fitfunc(p, x)
+								err_indices = vfunc(y, model)
+								err_range = np.arange(len(err_indices))
+								print err_indices, err_range
+								err = verr(err_indices, err2)
+								print err2, err
+								raw_input("prompt")
+								llh = ((y - model)/err)**2
+								return llh
+							
+							pinit = [maxlum, -1.0]
+							out = optimize.leastsq(llh, pinit,
+							                       args=(newt, fity, fitsig), full_output=1)
+							
+							pfinal = out[0]
+							covar = out[1]
+							covars.append(covar)
+							params.append(pfinal)
+
+							vals.append(np.sum(llh(pfinal, newt, fity,fitsig)))
+						
+#						print vals
+						
+						ax2 = plt.subplot(311)
+						
+						
+						
+						best = min(vals)
+						index = vals.index(best)
+						time = maxtime - (offset[index] + 1e-5)
+
+						lower=1.
+						upper=(maxtime-lastt)
+						mid=False
+						
+						for j in range(len(vals)):
+							v = vals[j]
+							if not mid:
+								if v > (0.5+best):
+									lower = offset[j]
+								else:
+									mid = True
+							else:
+								if v > (0.5+best):
+									upper = offset[j]
+									break
+								
+						print "Done!", lower, upper, offset[index], (lower+upper)/2, (upper-lower)
+						
+						plt.plot(offset, np.array(vals)-best)
+						plt.xlabel("Log(Age of TDE at observed maximum brightness)")
+						plt.ylabel(r"$\Delta$ Log Likelihood (Gaussian)")
+						ax2.set_xscale("log")
+						
+						plt.axvspan(lower, upper, color="r", alpha=0.3)
+						plt.scatter(offset[index], 0.0, color="r", marker="*")
+						plt.axhline(0.5, color="g")
+
+						bestparams = params[index]
+						covar = covars[index]
+						
+#						print best, pfinal, offset[index]
+#						raw_input("prompt")
+
+						plottimes = np.linspace(fitt[0], fitt[-1], 200)
+						shiftedtimes = plottimes-time
+						
+						def fit(x):
+							return bestparams[0] * (x**bestparams[1])
+						
+						plt.title("Minimum occurs at " + str(int(time)) + " MJD (" + str(int(offset[index]))+" days before date of observed maximum, with 1 " r"$\sigma$ range of " + str(int(lower)) + " to "+ str(int(upper)) + " days before maximum)")							
+						if type(covar) != type(None):							
+							ax1.annotate("Best fit has power law with gradient " + "{0:.3g}".format(bestparams[1]) + " +/- " + str(math.sqrt(np.abs(covar[1][1]))), xy=(0.05, 0.1), xycoords="axes fraction")
+						else:
+							ax1.annotate("Best fit has power law with gradient " + "{0:.3g}".format(bestparams[1]) + " but failed to find covariance (fit did not converge correctly)", xy=(0.05, 0.1), xycoords="axes fraction")
+							
+						ax3 = plt.subplot(313)
+						ax3.errorbar(fitt-time, fity, yerr=[fitup, fitdown], label="measurements", color="b", ecolor="r", fmt='o')
+						ax3.plot(shiftedtimes, fit(shiftedtimes)) 
+						ax1.plot(plottimes, fit(shiftedtimes))
+						plt.ylabel("Log(Luminosity)")
+						plt.xlabel("Log(t-"+ str(int(time))+" MJD)")
+						
+						ax3.set_yscale('log')
+						ax3.set_xscale('log')
+							
+						fig.set_size_inches(15, 20)						
+						
+						path = "graphs/xrays/" + self.name+ ".pdf"						
+						
+						plt.savefig(path)
+						plt.close()
+						print "Saving to", path
+
 				
 	def spectrum(self):
+		"""Checks to see if the candidate has spectral data.
+		"""
 		if hasattr(self, "spectra"):
 			if self.spectra.u_fluxes != "Uncalibrated":
 				self.has_spectra = True
 			
 	def plot_spectrum(self):
+		"""Plots the spectral data for a given candidate.
+		"""
 		if self.has_spectra:
 			plt.figure()
 			
@@ -469,6 +707,8 @@ class TDE_Candidate:
 			"No photometry was found for", self.name
 		
 def scatter_plot(folder, title, allt, ally, allupt, allupy,labels, variables, maxdate, sharex=False):
+	"""Produces scatter plots for a given set of data.
+	""" 	
 	fig = plt.figure()
 	plt.suptitle(title)
 	npoints = len(labels)
@@ -495,11 +735,13 @@ def scatter_plot(folder, title, allt, ally, allupt, allupy,labels, variables, ma
 			
 			if md[1] != None:
 				plt.axvline(md[1], color = "r", label="max vis date")
-
+			
 			if len(upt) > 0:
 				arrow = u'$\u2193$'
 				plt.scatter(upt, upy, marker=arrow, s=300, label="upper limits")
 			
+			plt.scatter(t, y, label="measurements", color="orange")
+
 			plt.title(label)
 
 			if var == "magnitude":
@@ -508,7 +750,7 @@ def scatter_plot(folder, title, allt, ally, allupt, allupy,labels, variables, ma
 			elif var == "luminosity":
 				ax.set_yscale('log')
 
-			plt.scatter(t, y, label="measurements")
+			
 			
 			plt.xlabel("Time (MJD)")
 			plt.ylabel(var)
@@ -528,6 +770,8 @@ def scatter_plot(folder, title, allt, ally, allupt, allupy,labels, variables, ma
 		print allt, ally, labels, variables
 		
 def histograms_plot(titles, xlabels, values):
+	"""Plots histograms for a given set of variables
+	"""
 	fig = plt.figure()
 	npoints = len(titles)
 	nrows = int(float(npoints)/2.) + (npoints % 2)
@@ -549,6 +793,8 @@ def histograms_plot(titles, xlabels, values):
 	plt.close()
 				
 def get_degrees(time_str):
+	"""Converts a HH:MM:SS time string to a float (number of hours).
+	"""
 	if time_str.count(':') >1 :
 		s, arcm, arcs = time_str.split(':')
 		if float(s) < 0.0:
